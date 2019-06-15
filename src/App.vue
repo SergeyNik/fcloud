d38e7f37f3edc7be9459c3258e63f1af7d4c509c
sergei.nikolchikov <sergei.nikolchikov@itbrick.ru>
2019-06-15 22:47:09 +0300

    axios + vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app dark>
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
 <style>
@@ -29,3 +27,7 @@
   color: #42b983;
 }
 </style>
+<script>
+  export default {
+  }
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <app-header></app-header>
+        Home screen
+    </div>
+</template>
+
+<script>
+    import AppHeader from "@/components/AppHeader";
+
+    export default {
+        name: "Index",
+        components: {
+            AppHeader
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
